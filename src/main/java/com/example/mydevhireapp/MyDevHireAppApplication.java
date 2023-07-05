bb69cd94a74bddb09de3a352a16f9b9f0410c8cd
Sylwia <sylwia.szalacha1@gmail.com>
2023-07-06 00:10:20 +0200

    dependecies, readme

---
+++ src/main/java/com/example/mydevhireapp/MyDevHireAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.mydevhireapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDevHireAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyDevHireAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mydevhireapp/MyDevHireAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mydevhireapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDevHireAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
